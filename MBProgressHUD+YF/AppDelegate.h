1f24b70122a77438fd862575095420458ce022af
BigShow1949 <1029883589@qq.com>
2017-01-05 10:58:04 +0800

    重传文件

---
+++ MBProgressHUD+YF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBProgressHUD+YF
+//
+//  Created by zhht01 on 16/1/8.
+//  Copyright © 2016年 BigShow1949. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
